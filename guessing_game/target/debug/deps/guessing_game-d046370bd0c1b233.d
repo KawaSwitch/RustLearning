b0d4165e2dea05e1707f9c70d6d8d25a3cd54a24
KawaSwitch <bb35316013@ms.nagasaki-u.ac.jp>
2018-08-07 18:51:41 +0900

    数あてゲームのフォルダ追加

---
+++ guessing_game/target/debug/deps/guessing_game-d046370bd0c1b233.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/KarutaSwitch/Desktop/rust/guessing_game/target/debug/deps/guessing_game-d046370bd0c1b233: src/main.rs
+
+/mnt/c/Users/KarutaSwitch/Desktop/rust/guessing_game/target/debug/deps/guessing_game-d046370bd0c1b233.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/KarutaSwitch/Desktop/rust/guessing_game/target/debug/guessing_game: /mnt/c/Users/KarutaSwitch/Desktop/rust/guessing_game/src/main.rs
