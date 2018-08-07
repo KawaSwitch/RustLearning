4a39c8e299f836e447a610bea1c9015cb2907002
KawaSwitch <bb35316013@ms.nagasaki-u.ac.jp>
2018-08-07 18:37:54 +0900

    全体的なファイルの移動

--- hello_world/target/release/deps/hello_world-af7b72699786b6ea.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/KarutaSwitch/Desktop/rust/hello_world/target/release/deps/hello_world-af7b72699786b6ea: src/main.rs
-
-/mnt/c/Users/KarutaSwitch/Desktop/rust/hello_world/target/release/deps/hello_world-af7b72699786b6ea.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/release/hello_world.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/KarutaSwitch/Desktop/rust/hello_world/target/release/hello_world: /mnt/c/Users/KarutaSwitch/Desktop/rust/hello_world/src/main.rs
